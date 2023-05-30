6e5e17c645fa214e7dc09b959741dcd652b52892
Nikita <nikita@MacBook-Air-Nikita.local>
2023-05-30 10:46:55 +0300

    src

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Tester tester = new Tester("Nikita", "Holopov", 2, "B2", 1300);
+        tester.getFullName();
+        tester.print();
+    }
+}
